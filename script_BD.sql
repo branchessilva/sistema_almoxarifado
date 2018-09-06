fa900c8fa6178bccaae97e49068de33c8d66c90d
branchessilva <fabiobranches.fr@gmail.com>
2018-09-06 10:59:40 -0300

    Add files via upload
    Script do banco de dados em sql

---
+++ script_BD.sql
@@ -0,0 +1,49 @@
+
+create table setor(
+	cod_setor int primary key not null auto_increment,
+    nome varchar(45) not null
+);
+
+create table perfil (
+	cod_perfil int primary key not null auto_increment,
+    nome varchar(5) not null
+);
+
+create table usuario(
+	cod_usuario int primary key not null auto_increment,
+    matricula int not null,
+    senha varchar(20) not null,
+    fk_Setor int, 
+    fk_Perfil int,
+    constraint fk_setor foreign key (fk_Setor) references setor (cod_setor),
+    constraint fk_perfil foreign key (fk_Perfil) references perfil (cod_perfil)
+);
+
+create table pedido(
+	cod_pedido int primary key not null auto_increment,
+	recebedor varchar(45) not null,
+    autorizador varchar(45) not null,
+    solicitante varchar(45) not null,
+    data_Pedido date not null,
+    fk_Usuario int,
+    fk_Estado int,
+    constraint fk_estado foreign key (fk_Estado) references estado (cod_estado),
+    constraint fk_usuario foreign key (fk_Usuario) references usuario (cod_usuario)
+);
+
+create table itens(
+	cod_item int primary key not null,
+    nome varchar(45) not null,
+    quantidade_Estoque int,
+    unidade_Tipo varchar (10) not null
+);
+
+create table pedido_Item(
+    fk_Pedido int,
+    fk_Item int,
+	constraint fk_pedido foreign key (fk_Pedido) references pedido (cod_pedido),
+    constraint fk_item foreign key (fk_Item) references itens (cod_item),
+	constraint  cod_Pedido_Item primary key (fk_Pedido, fk_Item),
+    quantidade_Solicitada int not null,
+    quantidade_Fornecida int not null
+);
